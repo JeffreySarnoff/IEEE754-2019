7b761a475bc2ef8162df1a763ed42f73aca229c2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-08-12 23:52:58 -0400

    Create IEEE754-2019.jl

---
+++ src/IEEE754-2019.jl
@@ -0,0 +1,11 @@
+"""
+    IEEE754-2019
+    
+Provide performant, conforming implementations of functions recommended within the 2019 revision of IEEE754.
+"""
+module IEEE754-2019
+
+
+
+
+end  # IEEE754-2019
